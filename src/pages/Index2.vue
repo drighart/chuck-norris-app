96dc6646f8db833426e141865d310cff8727125c
drighart <coder@a6ab96c3c996>
2019-09-17 17:18:53 +0000

    initial commit

---
+++ src/pages/Index2.vue
@@ -0,0 +1,27 @@
+import { Platform } from 'quasar'
+import { uid } from 'quasar'
+
+<template>
+  <q-page class="flex flex-center">
+    {{ $q.platform }} {{ $q.uuid }}
+    <q-btn color="purple" to="/map">
+      <q-icon left size="3em" name="map" />
+      <div>Try this!</div>
+    </q-btn>
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      myInfiniteVariable: true,
+      uuid: 'hello'
+    }
+  }
+}
+</script>
