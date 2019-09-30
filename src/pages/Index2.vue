347de9c2c2f0938fd5f6cac1df28cde4dbd0c965
drighart <coder@a6ab96c3c996>
2019-09-30 12:57:14 +0000

    button

--- src/pages/Index2.vue
+++ src/pages/Index2.vue
@@ -1,12 +1,12 @@
 import { Platform } from 'quasar'
 import { uid } from 'quasar'
+import { axios } from 'axios'
 
 <template>
   <q-page class="flex flex-center">
-    {{ $q.platform }} {{ $q.uuid }}
-    <q-btn color="purple" to="/map">
-      <q-icon left size="3em" name="map" />
-      <div>Try this!</div>
+    <q-btn color="purple" @click="loadData()">
+      <q-icon left size="3em" name="map"/>
+      <div>Press the Chuck Norris button!</div>
     </q-btn>
   </q-page>
 </template>
@@ -17,10 +17,31 @@ import { uid } from 'quasar'
 <script>
 export default {
   name: 'PageIndex',
-  data () {
-    return {
-      myInfiniteVariable: true,
-      uuid: 'hello'
+  methods: {
+    showNotification () {
+      this.$q.notify('Some other message')
+    },
+    loadData () {
+      this.$axios.get('https://api.chucknorris.io/jokes/random')
+        .then((response) => {
+          this.data = response.data
+          this.chucky = this.data.value
+          this.$q.notify({
+            color: 'purple',
+            textColor: 'white', // if default 'white' doesn't fit
+            position: 'top',
+            message: this.chucky,
+            icon: 'wifi'
+          })
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Loading failed',
+            icon: 'report_problem'
+          })
+        })
     }
   }
 }
